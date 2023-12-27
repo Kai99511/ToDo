31a513bbba6946526dfbbfdcbc23d4b98a0f94de
Kai <kaitomo51199@gmail.com>
2023-12-27 17:12:01 +0900

    update:Ul->ul

--- TODOapp.js
+++ TODOapp.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-const Ul = document.getElementById("ul");
+const ul = document.getElementById("ul");
 
 const todos = JSON.parse(localStorage.getItem("todos"));
 
@@ -17,13 +17,13 @@ form.addEventListener("submit", function (event) {
 
 //入力された値をリストに追加
 function add(todo) {
-    let todoText = input.value;
+    let todoText = input.value;//valueフォームの値を取り出す
 
     if (todo) {
         todoText = todo.text;
     }
 
-    if (todoText.length > 0) //0文字以上の値が入力された場合true (length>0)はなくても良い暗黙的型変換
+    if (todoText.length > 0) //0文字以上の値が入力された場合true (length>0)はなくても良い。暗黙的型変換
     {
         const li = document.createElement("li");
         li.innerText = todoText;
@@ -40,12 +40,11 @@ function add(todo) {
         });
         //左クリックで取り消し線をつける
         li.addEventListener("click", function () {
-            li.classList.toggle
-                ("text-decoration-line-through")//toggleなければつける。あれば消す
+            li.classList.toggle//toggleなければつける。あれば消す
+                ("text-decoration-line-through")
             saveData();
         });
-
-        Ul.appendChild(li);//ulタグの子供としてタグ追加できる
+        ul.appendChild(li);//ulタグの子供としてタグ追加できる
         input.value = "";//入力フォームを空にする
         saveData();
     }
@@ -54,7 +53,7 @@ function add(todo) {
 //TODOの記録
 function saveData() {
     const lists = document.querySelectorAll("li");//liタグをすべて取ってくる
-    let todos = [];
+    let todos = [];//配列
 
     //例:forEach
     //const array = [1,2,3]
